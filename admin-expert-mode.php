93280ce0b872f131ff293465ef6493616a9eec3e
Scott Reilly <scott@coffee2code.com>
2010-08-19 14:17:33 -0400

    Release v1.5:
    * Display notice on plugin's activation to remind admin that expert mode must be enabled for each user before it takes effect for the user
    * Allow configuring of default expert mode state for all users, via 'c2c_admin_expert_mode_default' filter (initially set to false)
    * Allow enabling of expert mode for all users, via 'c2c_admin_expert_mode' filter (having it return true)
    * Add true localization support
    * Handle text on 'Install Themes' page
    * Handle text on 'Add New Plugins'
    * Check for `is_admin()` before defining class rather than during constructor
    * Add function `is_admin_expert_mode_active()` to encapsulate logic to determine if expert mode is active for the current user
    * Change plugin description
    * Instantiate object within primary `class_exists()` check
    * Assign object instance to global variable `$c2c_admin_expert_mode` to allow for external manipulation
    * Add PHPDoc documentation
    * Note compatibility with WP 2.9+ and 3.0+
    * Drop compatibility with versions of WP older than 2.8
    * Minor code reformatting (spacing)
    * Remove docs from top of plugin file (all that and more are in readme.txt)
    * Remove trailing whitespace in header docs
    * Add Changelog, Upgrade Notice, and Filters sections to readme.txt
    * Fix sublist syntax usage in readme.txt
    * Add package info to top of plugin file
    * Update copyright date

--- admin-expert-mode.php
+++ admin-expert-mode.php
@@ -1,70 +1,23 @@
 <?php
+/**
+ * @package Admin_Expert_Mode
+ * @author Scott Reilly
+ * @version 1.5
+ */
 /*
 Plugin Name: Admin Expert Mode
-Version: 1.2
-Plugin URI: http://coffee2code.com/wp-plugins/admin-expert-mode
+Version: 1.5
+Plugin URI: http://coffee2code.com/wp-plugins/admin-expert-mode/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
-Description: Allow users with access to the adminstration section to hide inline documentation and help text, which generally target beginning users.
-
-WordPress 2.7 introduced a variety of inline documentation that provide explanations for various feature and input fields 
-(i.e. things like explanations of excerpts, trackbacks, custom fields, page parents, etc).  These are great for newcomers
-to WordPress.  For those sufficiently familiar with WordPress, these bits of text are no longer necessary and merely
-provide visual noise.  This plugin gets rid of those descriptive texts.
-
-This plugin's behavior is made available as a per-user profile option.  Each user who wishes to enable expert mode for
-themselves must do so individually by going into their profile, checking the field 'Expert mode', and then pressing the
-'Update Profile' button.
-
-Specifically, it removes:
-	- Categories
-		* Description of "Category Name"
-		* Description of "Category Slug"
-		* Description of "Category Parent"
-		* Description of "Description"
-	- Edit Post
-		* Description of "Custom Fields"
-		* Description of "Excerpts"
-		* Description of "Trackbacks"
-	- Edit Page
-		* Description of comment status
-		* Description of "Custom Fields"
-		* Verbose descriptions of "Attributes" (parent, template, and order)
-	- Edit Link
-		* Description of "Name"
-		* Description of "Web Address"
-		* Description of "Description"
-		* Description of "Target"
-		* Description of "Link Relationships (XFN)"
-	- Tags
-		* Description of "Tag name"
-		* Description of "Tag slug"
-	- Widgets
-		* Text indicating that widgets are added from the listing of widgets on the left.
-	- Install Themes
-		* Help text for search field
-		* Help text for "Feature Filter"
-	- Manage Plugins
-		* Description of what plugins are and that they are activated/deactivated on that page
-		* Text indicating that broken plugins can be renamed to remove them from being active
-		* Description of the "Recently Active Plugins" section
-	- Add New Plugins
-		* Help text for search field
-		* Help text for "Popular Tags"
-		
+Text Domain: admin-expert-mode
+Description: Allow users with access to the administration section to hide inline documentation and help text, which generally target beginning users.
 
 Compatible with WordPress 2.8+, 2.9+, 3.0+.
 
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/admin-expert-mode.zip and unzip it into your
-/wp-content/plugins/ directory.
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
-3. Each user who wishes to enable expert mode must do so individually by going into their profile, checking the field labeled 'Expert mode', and then pressing the 'Update Profile' button.
-
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/admin-expert-mode/
 */
 
 /*
@@ -88,54 +41,85 @@ if ( is_admin() && !class_exists( 'AdminExpertMode' ) ) :
 class AdminExpertMode {
 	var $admin_options_name = 'c2c_admin_expert_mode';
 	var $field_name = 'admin_expert_mode';
+	var $textdomain = 'admin-expert-mode';
+	var $textdomain_subdir = 'lang';
 	var $prompt = '';
 	var $help_text = '';
 	var $config = array();
 	var $activating = false;
 	var $is_active = false; // Has admin expert mode been determined to be active?
-	var $is_filtered_true = false; // Was admin expert mode activated via add_filter()?
 
+	/**
+	 * Constructor
+	 */
 	function AdminExpertMode() {
 		global $pagenow;
 
-		$this->prompt = __( 'Expert mode' );
-		$this->help_text = __( 'Enable expert mode if you are familiar with WordPress and don\'t need the inline documentation in the admin.' );
-		$this->config[$this->field_name] = 0; // Admin Expert Mode is assumed to be initially off
+		$this->prompt = __( 'Expert mode', $this->textdomain );
+		$this->help_text = __( 'Enable expert mode if you are familiar with WordPress and don\'t need the inline documentation in the admin.', $this->textdomain );
 
 		add_action( 'admin_head', array( &$this, 'add_css' ) );
 		add_action( 'admin_notices', array( &$this, 'display_activation_notice' ) );
 		add_action( 'activate_'.str_replace( trailingslashit( dirname( dirname( __FILE__ ) ) ), '', __FILE__ ), array( &$this, 'plugin_activated' ) );
+		add_action( 'init', array( &$this, 'load_textdomain' ) );
 		if ( 'profile.php' == $pagenow ) {
-			add_action( 'admin_init', array(&$this, 'maybe_save_options' ) );
-			add_action( 'profile_personal_options', array(&$this, 'show_option' ) );
+			add_action( 'admin_init', array( &$this, 'maybe_save_options' ) );
+			add_action( 'profile_personal_options', array( &$this, 'show_option' ) );
 		}
 	}
 
-	function plugin_activated() {
-		set_transient( 'aem_activated', "show", 10 );
+	/**
+	 * Loads the localization textdomain for the plugin.
+	 *
+	 * @return void
+	 */
+	function load_textdomain() {
+		$subdir = empty( $this->textdomain_subdir ) ? '' : '/'.$this->textdomain_subdir;
+		load_plugin_textdomain( $this->textdomain, false, basename( dirname( __FILE__ ) ) . $subdir );
 	}
 
+	/**
+	 * Set a temporary flag (transient) to indicate the plugin was just activated.
+	 *
+	 * @return void
+	 */
+	function plugin_activated() {
+		set_transient( 'aem_activated', 'show', 10 );
+	}
+
+	/**
+	 * Output activation notice
+	 *
+	 * @return void (Text is echoed.)
+	 */
 	function display_activation_notice() {
 		if ( get_transient( 'aem_activated' ) ) {
-			$msg = sprintf( __( '<strong>NOTE:</strong> (Note that you must enable expert mode for yourself (in your <a href="%s" title="Profile">profile</a>) and/or any other user accounts for it to take effect.' ), admin_url( 'profile.php' ) );
+			$msg = sprintf( __( '<strong>NOTE:</strong> You must enable expert mode for yourself (in your <a href="%s" title="Profile">profile</a>) for it to take effect. Other admin users must do the same for themselves as well. (See the readme.txt for more advanced controls.)', $this->textdomain ), admin_url( 'profile.php' ) );
 			echo "<div id='message' class='updated fade'><p>$msg</p></div>";
 		}
 	}
 
+	/**
+	 * Indicates if admin expert mode is active for the current user.
+	 *
+	 * Takes the following into account in this order
+	 * * Value of 'c2c_admin_expert_mode' filter, if true
+	 * * Value of the per-user setting
+	 *
+	 * @return void (Text is echoed.)
+	 */
 	function is_admin_expert_mode_active() {
-		if ( $this->is_active )
-			return true;
-		if ( apply_filters( 'c2c_admin_expert_mode', false ) || apply_filters( 'c2c_admin_expert_mode_' . get_user_option( 'user_login' ), false ) ) {
+		$options = $this->get_options();
+		if ( $this->is_active || apply_filters( 'c2c_admin_expert_mode', $options[$this->field_name], get_user_option( 'user_login' ) ) )
 			$this->is_active = true;
-			$this->is_filtered_true = true;
-		} else {
-			$options = $this->get_options();
-			if ( $options[$this->field_name] )
-				$this->is_active = true;
-		}
 		return $this->is_active;
 	}
 
+	/**
+	 * Outputs CSS if the user has admin expert mode activated.
+	 *
+	 * @return void (Text is echoed.)
+	 */
 	function add_css() {
 		if ( !$this->is_admin_expert_mode_active() )
 			return;
@@ -149,13 +133,18 @@ class AdminExpertMode {
 		#linkxfndiv .inside > p, #addressdiv .inside > p,
 		#current-widgets-head #sidebar-info p:last-child,
 		#icon-plugins + h2 + p, #currently-active + form + p, #recent-plugins + p, #inactive-plugins + form + h2 + p + p + p,
-		#addcat .form-field p, #addtag .form-field p, .install-help { display:none; }
+		#addcat .form-field p, #addtag .form-field p, .edit-tags-php #col-right .form-wrap, .install-help { display:none; }
 		</style>
 
 CSS;
 	}
 
-	function show_option($user) {
+	/**
+	 * Outputs the form input field for the admin expert mode setting checkbox.
+	 *
+	 * @return void (Text is echoed.)
+	 */
+	function show_option( $user ) {
 		$options = $this->get_options();
 		$checked = $options[$this->field_name] ? ' checked="checked"' : '';
 		echo <<<HTML
@@ -169,14 +158,25 @@ CSS;
 HTML;
 	}
 
+	/**
+	 * Returns array of the plugin's settings.
+	 *
+	 * @return array The plugin's settings
+	 */
 	function get_options() {
 		if ( !empty( $this->options ) )
 			return $this->options;
 		$existing_options = get_user_option( $this->admin_options_name );
-		$this->options = wp_parse_args( $existing_options, $this->config );
+		$default = apply_filters( 'c2c_admin_expert_mode_default', false );
+		$this->options = wp_parse_args( $existing_options, array( $this->field_name => $default ) );
 		return $this->options;
 	}
 
+	/**
+	 * Saves the user setting.
+	 *
+	 * @return void
+	 */
 	function maybe_save_options() {
 		$user = wp_get_current_user();
 		if ( isset( $_POST['submit'] ) ) {
@@ -189,9 +189,8 @@ HTML;
 
 } // end AdminExpertMode
 
-endif; // end if !class_exists()
+$GLOBALS['c2c_admin_expert_mode'] = new AdminExpertMode();
 
-if ( class_exists( 'AdminExpertMode' ) )
-	new AdminExpertMode();
+endif; // end if !class_exists()
 
 ?>
--- readme.txt
+++ readme.txt
@@ -4,14 +4,15 @@ Donate link: http://coffee2code.com/donate
 Tags: admin, expert, help, remove inline documentation, coffee2code
 Requires at least: 2.8
 Tested up to: 3.0.1
-Stable tag: 1.2
-Version: 1.2
+Stable tag: 1.5
+Version: 1.5
 
-Allow users with access to the adminstration section to hide inline documentation and help text, which generally target beginning users.
+Allow users with access to the administration section to hide inline documentation and help text, which generally target beginning users.
+
 
 == Description ==
 
-Allow users with access to the adminstration section to hide inline documentation and help text, which generally target beginning users.
+Allow users with access to the administration section to hide inline documentation and help text, which generally target beginning users.
 
 WordPress 2.7 introduced a variety inline documentation that provide explanations for various feature and input fields (i.e. things like explanations of excerpts, trackbacks, custom fields, page parents, etc).  These are great for newcomers to WordPress.  For those sufficiently familiar with WordPress, these bits of text are no longer necessary and merely provide visual noise.  This plugin gets rid of those descriptive texts.
 
@@ -50,46 +51,99 @@ Specifically, this plugin removes:
 * Widgets
     * Text indicating that widgets are added from the listing of widgets on the left.
 
+* Install Themes
+    * Help text for search field
+    * Help text for "Feature Filter"
+
 * Manage Plugins
     * Description of what plugins are and that they are activated/deactivated on that page
     * Text indicating that broken plugins can be renamed to remove them from being active
     * Description of the "Recently Active Plugins" section
+
+* Add New Plugins
+    * Help text for search field
+    * Help text for "Popular Tags"
 
 NOTE: This plugin does NOT remove input field labels or section headers, nor anything containing actual data.
 
 
 == Installation ==
 
-1. Unzip `admin-expert-mode.zip` inside the `/wp-content/plugins/` directory for your site
+1. Unzip `admin-expert-mode.zip` inside the `/wp-content/plugins/` directory for your site (or install via the built-in WordPress plugin installer)
 1. Activate the plugin through the 'Plugins' admin menu in WordPress
 1. Each user who wishes to enable expert mode for themselves must do so individually by going into their profile, checking the field 'Expert mode', and then pressing the 'Update Profile' button.
 
+
+== Filters ==
+
+The plugin is further customizable via two filters. Typically, these customizations would be put into your active theme's functions.php file, or used by another plugin.
+
+= c2c_admin_expert_mode =
+
+The 'c2c_admin_expert_mode' filter allows you to dynamically determine whether the admin expert mode should be active.
+
+Arguments:
+
+* $is_active (bool): Boolean indicating if admin expert mode is currently active
+* $user_login (string): Login of the current user
+
+Example:
+
+`<?php add_filter( 'c2c_admin_expert_mode', 'aem_never_let_bob_activate', 10, 2 );
+// Never let user 'bob' activate admin expert mode
+function aem_never_let_bob_activate( $is_active, $user_login ) {
+	if ( 'bob' == $user_login )
+		return false;
+	return $is_active; // Otherwise, preserve activation status for user
+} ?>`
+
+= c2c_admin_expert_mode_default =
+
+The 'c2c_admin_expert_mode_default' filter allows you to specify whether admin expert mode should be active for users by default or not.  This filter only applies to users who visit the admin for the first time after the plugin is activated.  Once a user visits the admin, their setting gets set to the default state and will no longer be affected by this filter.  If you wish to affect the setting for existing users, use the 'c2c_admin_expert_mode' filter instead.
+
+Arguments:
+
+* $is_active (bool): Boolean indicating if admin expert mode is active by default (default is false)
+
+Example (only valid in WP 3.0+):
+
+`<?php // Enable admin expert mode for all users by default
+add_filter( 'c2c_admin_expert_mode_default', '__return_true' );
+?>`
+
+
 == Changelog ==
 
-= ALPHA =
-* Check for is_admin() before defining class rather than during constructor
-* Minor code reformatting (spacing)
-
-= 1.2 =
+= 1.5 =
 * Display notice on plugin's activation to remind admin that expert mode must be enabled for each user before it takes effect for the user
+* Allow configuring of default expert mode state for all users, via 'c2c_admin_expert_mode_default' filter (initially set to false)
 * Allow enabling of expert mode for all users, via 'c2c_admin_expert_mode' filter (having it return true)
-* Allow enabling of expert mode on per user basis, via 'c2c_admin_expert_mode_$user' filter (if it returns true) (where $user is the login name of the user)
+* Add true localization support
 * Handle text on 'Install Themes' page
 * Handle text on 'Add New Plugins'
+* Check for is_admin() before defining class rather than during constructor
 * Add function is_admin_expert_mode_active() to encapsulate logic to determine if expert mode is active for the current user
 * Change plugin description
+* Instantiate object within primary class_exists() check
+* Assign object instance to global variable $c2c_admin_expert_mode to allow for external manipulation
+* Add PHPDoc documentation
 * Note compatibility with WP 2.9+ and 3.0+
-* Drop compatibility with versions of WP older than WP 2.8
+* Drop compatibility with versions of WP older than 2.8
+* Minor code reformatting (spacing)
+* Remove docs from top of plugin file (all that and more are in readme.txt)
+* Remove trailing whitespace in header docs
+* Add Changelog, Upgrade Notice, and Filters sections to readme.txt
+* Fix sublist syntax usage in readme.txt
+* Add package info to top of plugin file
 * Update copyright date
-* Fix sublist syntax usage in readme
-* Add Changelog to readme
 
 = 1.1 =
 * Additionally hide inline docs on categories and tags admin pages
-* Noted WP2.8 compatibility
+* Noted WP 2.8+ compatibility
 
 = 1.0 =
 * Initial release
+
 
 == Screenshots ==
 
@@ -98,3 +152,7 @@ NOTE: This plugin does NOT remove input field labels or section headers, nor any
 3. A screenshot of the plugin's profile checkbox on the user Profile page.
 
 
+== Upgrade Notice ==
+
+= 1.5 =
+Recommended update! Highlights: hides newly added help text, displays reminder on plugin activation, added filters, localization support, dropped pre-WP 2.8 compatibility, added verified WP 3.0 compatibility.
