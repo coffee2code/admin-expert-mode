497b084010edf046c60db011b439857932cc165c
Scott Reilly <scott@coffee2code.com>
2019-12-17 23:31:17 -0800

    Unit tests: Add tests for default hooks

--- tests/test-admin-expert-mode.php
+++ tests/test-admin-expert-mode.php
@@ -16,6 +16,19 @@ class Admin_Expert_Mode_Test extends WP_UnitTestCase {
 	//
 
 
+	public static function get_default_hooks() {
+		return array(
+			array( 'action', 'admin_init',               'register_styles',           10 ),
+			array( 'action', 'admin_notices',            'display_activation_notice', 10 ),
+			array( 'action', 'personal_options',         'show_option',               10 ),
+			array( 'action', 'personal_options_update',  'maybe_save_options',        10 ),
+			array( 'action', 'edit_user_profile_update', 'maybe_save_options',        10 ),
+			array( 'action', 'admin_enqueue_scripts',    'enqueue_admin_css',         10 ),
+		);
+	}
+
+
+
 	//
 	//
 	// TESTS
@@ -33,6 +46,19 @@ class Admin_Expert_Mode_Test extends WP_UnitTestCase {
 
 	public function test_hooks_plugins_loaded() {
 		$this->assertEquals( 10, has_action( 'plugins_loaded', array( 'c2c_AdminExpertMode', 'init' ) ) );
+	}
+
+	/**
+	 * @dataProvider get_default_hooks
+	 */
+	public function test_default_hooks( $hook_type, $hook, $function, $priority ) {
+		$prio = $hook_type === 'action' ?
+			has_action( $hook, array( 'c2c_AdminExpertMode', $function ) ) :
+			has_filter( $hook, array( 'c2c_AdminExpertMode', $function ) );
+		$this->assertNotFalse( $prio );
+		if ( $priority ) {
+			$this->assertEquals( $priority, $prio );
+		}
 	}
 
 }
